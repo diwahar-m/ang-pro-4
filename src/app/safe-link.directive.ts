3bea9ed21bcb83e3613fca7eddaa9aebb6639ef2
diwahar-m <msdiwahar5@gmail.com>
2026-09-03 18:53:12 +0530

    added directives for both functional & structural

--- src/app/safe-link.directive.ts
+++ src/app/safe-link.directive.ts
@@ -1,14 +1,19 @@
-import { Directive, input } from "@angular/core";
+import { Directive, ElementRef, inject, input } from "@angular/core";
+import { LogDirective } from "./log.directive";
 
 @Directive({
     selector: 'a[appSafeLink]',
     standalone: true,
     host: {
         '(click)': 'onConfirmLeavePage($event)'
-    }
+    },
+    hostDirectives: [LogDirective
+        
+    ]
 })
 export class SafeLinkDirective { 
-    queryParam = input('myapp');
+    queryParam = input('myapp'); 
+    private hostElementRef  = inject<ElementRef<HTMLAnchorElement>>(ElementRef);
 
     constructor() { 
         console.log('SafeLinkDirective is active!');
@@ -17,8 +22,8 @@ export class SafeLinkDirective {
     onConfirmLeavePage(event: MouseEvent) { 
         const wantsToLeave  = window.confirm('Do you want to leave the page?'); 
         if(wantsToLeave) {  
-            const address = (event.target as HTMLAnchorElement).href;
-            (event.target as HTMLAnchorElement).href = address + '?from='+ this.queryParam();
+            const address = this.hostElementRef.nativeElement.href;
+            this.hostElementRef.nativeElement.href = address + '?from='+ this.queryParam();
             return;
         }
 
